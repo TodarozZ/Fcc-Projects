06c3b384e9260acf76e9a60bc5b1e1707101f026
Fabio Todaro <ggstarter@outlook.it>
2025-11-04 16:00:33 +0100

    feat: aggiunto nuovo esercizio e costruito lo scheletro per passare il test. mancano step 10-11

---
+++ jsLabs/Dom LABS/football team cards/script.js
@@ -0,0 +1,72 @@
+const footballTeam = {
+  "team": "Juventus Future Stars",
+  "year": 2025,
+  "headCoach": "Massimiliano Allegri",
+  "players": [
+    {
+      "name": "Andrea Belotti",
+      "position": "forward",
+      "isCaptain": true
+    },
+    {
+      "name": "Dries Mertens",
+      "position": "forward",
+      "isCaptain": false
+    },
+    {
+      "name": "Federico Chiesa",
+      "position": "forward",
+      "isCaptain": false
+    },
+    {
+      "name": "Lorenzo Pellegrini",
+      "position": "midfielder",
+      "isCaptain": false
+    },
+    {
+      "name": "Miralem Pjanic",
+      "position": "midfielder",
+      "isCaptain": false
+    },
+    {
+      "name": "Paulo Dybala",
+      "position": "midfielder",
+      "isCaptain": false
+    },
+    {
+      "name": "Manuel Locatelli",
+      "position": "midfielder",
+      "isCaptain": false
+    },
+    {
+      "name": "Toni Kroos",
+      "position": "midfielder",
+      "isCaptain": false
+    },
+    {
+      "name": "Giorgio Chiellini",
+      "position": "defender",
+      "isCaptain": false
+    },
+    {
+      "name": "Leonardo Bonucci",
+      "position": "defender",
+      "isCaptain": false
+    },
+    {
+      "name": "Danilo",
+      "position": "defender",
+      "isCaptain": false
+    },
+    {
+      "name": "Wojciech Szczesny",
+      "position": "goalkeeper",
+      "isCaptain": false
+    },
+    {
+      "name": "Gianluigi Donnarumma",
+      "position": "goalkeeper",
+      "isCaptain": false
+    }
+  ]
+}
